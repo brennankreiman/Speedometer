472a9fea49912effe53248b2315bf8869a16dc12
Brennan Kreiman <brennankreiman@gmail.com>
2019-11-16 16:51:27 -0600

    update width attr

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const version = 50000000000099;
+const version = 50000000099099;
 const cachePrefix = 'speedometer-';
 const staticCacheName = `${cachePrefix}static-${version}`;
 const expectedCaches = [staticCacheName];
